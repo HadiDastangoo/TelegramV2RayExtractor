26965c0aceacd9e5b9fbebe7dca2ca6ee889f3b2
Hadi Dastangoo <h.dastangoo@gmail.com>
2023-07-16 14:39:00 +0330

    Update config.php
    define `SUB_JALALI_DATE` constant

--- config.php
+++ config.php
@@ -7,6 +7,7 @@ ini_set('error_log', 'errors.log');
 // default parameters
 define('TIME_ZONE',				'Asia/Tehran');
 define('SUB_DIR',				'subscriptions/');
+define('SUB_JALALI_DATE',       true);
 define('CHANNELS_DIR',			'channels/');
 define('CHANNELS_ASSETS_DIR',	'channels/assets/');
 define('CRON_LOG_ENABLED',		false);
